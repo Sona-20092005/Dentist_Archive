4a811e4173d0f3ed5c8ef546b8cbfc1aae96d1cd
Sona20092005 <sona.grigoryan.20092005@gmail.com>
2026-03-24 18:31:52 +0400

    Initial commit

---
+++ src/main/java/com/dentistarchive/DentistArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.dentistarchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentistArchiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DentistArchiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dentistarchive/DentistArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dentistarchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentistArchiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
